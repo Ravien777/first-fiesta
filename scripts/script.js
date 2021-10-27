e6ab6bca833e1b18388cd617b4fbaec627b67738
Ravien Sewpal <50817860+Ravien777@users.noreply.github.com>
2021-10-27 10:05:53 -0300

    Added a nav menu (mobile & desktop)

---
+++ scripts/script.js
@@ -0,0 +1,8 @@
+// function menu_toggle(id) {
+//   var items = document.getElementById(id);
+//   if (items.style.left == '-100%') {
+//     items.style.left = '0';
+//   } else {
+//     items.style.left = '-100%';
+//   }
+// }
